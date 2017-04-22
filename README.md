98709bee76ea4f1df26a1121c1f8c27f6da4d8cf
bennetgallein <info@team-scorpia.de>
2017-04-22 14:37:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Documentation of the Intranet Project
+
+This is the official documentation for the Intranet Project. It is written by all team Members.
---
+++ js/script.js
@@ -0,0 +1,31 @@
+(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+$(document).ready(function() {
+			// Show or hide the sticky footer button
+			$(window).scroll(function() {
+				if ($(this).scrollTop() > 200) {
+					$('.go-top').fadeIn(200);
+				} else {
+					$('.go-top').fadeOut(200);
+				}
+			});
+
+			// Animate the scroll to top
+			$('.go-top').click(function(event) {
+				event.preventDefault();
+
+				$('html, body').animate({scrollTop: 0}, 300);
+			})
+		});
