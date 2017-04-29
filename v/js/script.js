0e0e560737dbb75abce89f29ad9abaa934f31fbb
Bennet Gallein <bennet@intranetproject.net>
2017-04-29 17:45:27 +0200

    ssh; ftp; manual installation;

--- v/js/script.js
+++ v/js/script.js
@@ -1,31 +1,20 @@
-(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
+$(document).ready(function () {
+    // Show or hide the sticky footer button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.go-top').fadeIn(200);
+        } else {
+            $('.go-top').fadeOut(200);
+        }
+    });
+
+    // Animate the scroll to top
+    $('.go-top').click(function (event) {
+        event.preventDefault();
+
         $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+            scrollTop: 0
+        }, 300);
+    });
 });
-$(document).ready(function() {
-			// Show or hide the sticky footer button
-			$(window).scroll(function() {
-				if ($(this).scrollTop() > 200) {
-					$('.go-top').fadeIn(200);
-				} else {
-					$('.go-top').fadeOut(200);
-				}
-			});
-
-			// Animate the scroll to top
-			$('.go-top').click(function(event) {
-				event.preventDefault();
-
-				$('html, body').animate({scrollTop: 0}, 300);
-			})
-		});
+// Select all links with hashes
